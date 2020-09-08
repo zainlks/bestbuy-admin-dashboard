09f8db53328160051c161977af352bfaf0133699
Zain Lakhani <lakhani.zain786@gmail.com>
2020-09-08 00:17:12 -0400

    Added Product Order Table

--- as.php
+++ as.php
@@ -1,6 +1,10 @@
 <?php
-  $url = "https://marketplace.bestbuy.ca/api/account?api_key=333281a3-8a7a-4cca-a415-f73f3089b8af";
-  $response = file_get_contents($url);
-  $response = json_decode($response);
-  echo($response->payment_details->paid_balance);
+// $url = "https://marketplace.bestbuy.ca/api/orders?api_key=333281a3-8a7a-4cca-a415-f73f3089b8af";
+// $response = file_get_contents($url);
+// $response = json_decode($response);
+// print_r($response->orders[0]->order_lines[0]->product_title);
+$date = date("Y-m-d\T");
+$url = "https://marketplace.bestbuy.ca/api/orders?api_key=333281a3-8a7a-4cca-a415-f73f3089b8af&start_date=$date";
+$fullURL = $url."00:00:00N";
+echo($fullURL);
 ?>
--- index.php
+++ index.php
@@ -794,8 +794,10 @@
               <div class="inner">
                 <h3>
                   <?php
-                    $url = "https://marketplace.bestbuy.ca/api/orders?api_key=333281a3-8a7a-4cca-a415-f73f3089b8af&start_date=2020-09-07T00:00:00Z";
-                    $response = file_get_contents($url);
+                    $date = date("Y-m-d\T");
+                    $url = "https://marketplace.bestbuy.ca/api/orders?api_key=333281a3-8a7a-4cca-a415-f73f3089b8af&start_date=$date";
+                    $fullURL = $url."00:00:00Z";
+                    $response = file_get_contents($fullURL);
                     $response = json_decode($response);
                     echo $response->total_count;
                   ?>
@@ -929,7 +931,7 @@
             <!-- Custom tabs (Charts with tabs)-->
             <div class="card">
               <div class="card-header border-transparent">
-                <h3 class="card-title">Latest Orders</h3>
+                <h3 class="card-title">Most Recent Orders</h3>
 
                 <div class="card-tools">
                   <button type="button" class="btn btn-tool" data-card-widget="collapse">
@@ -946,69 +948,62 @@
                   <table class="table m-0">
                     <thead>
                     <tr>
-                      <th>Order ID</th>
-                      <th>Item</th>
+                      <th>Product SKU</th>
+                      <th>Item Name</th>
                       <th>Status</th>
-                      <th>Popularity</th>
+                      <th>Price</th>
                     </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                      <td><a href="pages/examples/invoice.html">OR9842</a></td>
-                      <td>Call of Duty IV</td>
-                      <td><span class="badge badge-success">Shipped</span></td>
-                      <td>
-                        <div class="sparkbar" data-color="#00a65a" data-height="20">90,80,90,-70,61,-83,63</div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td><a href="pages/examples/invoice.html">OR1848</a></td>
-                      <td>Samsung Smart TV</td>
-                      <td><span class="badge badge-warning">Pending</span></td>
-                      <td>
-                        <div class="sparkbar" data-color="#f39c12" data-height="20">90,80,-90,70,61,-83,68</div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td><a href="pages/examples/invoice.html">OR7429</a></td>
-                      <td>iPhone 6 Plus</td>
-                      <td><span class="badge badge-danger">Delivered</span></td>
-                      <td>
-                        <div class="sparkbar" data-color="#f56954" data-height="20">90,-80,90,70,-61,83,63</div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td><a href="pages/examples/invoice.html">OR7429</a></td>
-                      <td>Samsung Smart TV</td>
-                      <td><span class="badge badge-info">Processing</span></td>
-                      <td>
-                        <div class="sparkbar" data-color="#00c0ef" data-height="20">90,80,-90,70,-61,83,63</div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td><a href="pages/examples/invoice.html">OR1848</a></td>
-                      <td>Samsung Smart TV</td>
-                      <td><span class="badge badge-warning">Pending</span></td>
-                      <td>
-                        <div class="sparkbar" data-color="#f39c12" data-height="20">90,80,-90,70,61,-83,68</div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td><a href="pages/examples/invoice.html">OR7429</a></td>
-                      <td>iPhone 6 Plus</td>
-                      <td><span class="badge badge-danger">Delivered</span></td>
-                      <td>
-                        <div class="sparkbar" data-color="#f56954" data-height="20">90,-80,90,70,-61,83,63</div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td><a href="pages/examples/invoice.html">OR9842</a></td>
-                      <td>Call of Duty IV</td>
-                      <td><span class="badge badge-success">Shipped</span></td>
-                      <td>
-                        <div class="sparkbar" data-color="#00a65a" data-height="20">90,80,90,-70,61,-83,63</div>
-                      </td>
-                    </tr>
+                    <?php
+                      $popProductArray = array();
+                      $mostPopularProducts = array();
+                      $popProductCount = [0,0,0,0,0,0,0,0,0];
+
+                      // if($popProductArray['12212']) {
+                      //   $popProductArray['12212'] =  7;
+                      // }
+                      $orders = array();
+
+                      // echo($popProductArray['12212']);
+                      $url = "https://marketplace.bestbuy.ca/api/orders?api_key=77b45e35-6555-43f4-bf38-cdac16625fba&max=100&sort=dateCreated&order=desc";
+                      $response = file_get_contents($url);
+                      $response = json_decode($response);
+
+                      for($x = 0;$x<25;$x++) {
+                        echo("<tr>
+                                <td><a href=https://www.bestbuy.ca/en-ca/search?search=");
+                                echo($response->orders[$x]->order_lines[0]->product_sku);
+                                echo(">");echo($response->orders[$x]->order_lines[0]->product_sku);
+
+                                echo("</a></td><td>");
+                                echo($response->orders[$x]->order_lines[0]->product_title);
+                                echo("</td>");
+                                if($response->orders[$x]->order_state == "WAITING_DEBIT" or $response->orders[$x]->order_state == "WAITING_DEBIT_PAYMENT") {
+                                  echo("<td><span class=\"badge badge-warning\">Debiting</span></td>");
+                                }
+                                elseif( $response->orders[$x]->order_state == "WAITING_ACCEPTANCE") {
+                                  echo("<td><span class=\"badge badge-info\">Waiting for Acceptance</span></td>");
+                                }
+                                elseif($response->orders[$x]->order_state == "SHIPPING" or $response->orders[$x]->order_state == "SHIPPED"){
+                                  echo("<td><span class=\"badge badge-danger\">Shipped</span></td>");
+                                }
+                                elseif($response->orders[$x]->order_state == "RECEIVED") {
+                                  echo("<td><span class=\"badge badge-success\">Recieved</span></td>");
+                                }
+                                elseif($response->orders[$x]->order_state == "CLOSED" or $response->orders[$x]->order_state == "REFUSED" or $response->orders[$x]->order_state == "WAITING" or $response->orders[$x]->order_state == "CANCELED") {
+                                  echo("<td><span class=\"badge badge-info\">Other</span></td>");
+                                }
+                                echo("</td><td>");
+                                echo("$");
+                                echo($response->orders[$x]->order_lines[0]->price);
+                                echo("</td></tr>");
+                      }
+
+
+                      // echo($response->orders[0]->total_price);
+
+                    ?>
                     </tbody>
                   </table>
                 </div>
@@ -1024,208 +1019,6 @@
             <!-- /.card -->
           </div>
           <!-- /.col -->
-
-            <!-- DIRECT CHAT -->
-            <div class="card direct-chat direct-chat-primary">
-              <div class="card-header">
-                <h3 class="card-title">Direct Chat</h3>
-
-                <div class="card-tools">
-                  <span title="3 New Messages" class="badge badge-primary">3</span>
-                  <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                    <i class="fas fa-minus"></i>
-                  </button>
-                  <button type="button" class="btn btn-tool" title="Contacts" data-widget="chat-pane-toggle">
-                    <i class="fas fa-comments"></i>
-                  </button>
-                  <button type="button" class="btn btn-tool" data-card-widget="remove">
-                    <i class="fas fa-times"></i>
-                  </button>
-                </div>
-              </div>
-              <!-- /.card-header -->
-              <div class="card-body">
-                <!-- Conversations are loaded here -->
-                <div class="direct-chat-messages">
-                  <!-- Message. Default to the left -->
-                  <div class="direct-chat-msg">
-                    <div class="direct-chat-infos clearfix">
-                      <span class="direct-chat-name float-left">Alexander Pierce</span>
-                      <span class="direct-chat-timestamp float-right">23 Jan 2:00 pm</span>
-                    </div>
-                    <!-- /.direct-chat-infos -->
-                    <img class="direct-chat-img" src="dist/img/user1-128x128.jpg" alt="message user image">
-                    <!-- /.direct-chat-img -->
-                    <div class="direct-chat-text">
-                      Is this template really for free? That's unbelievable!
-                    </div>
-                    <!-- /.direct-chat-text -->
-                  </div>
-                  <!-- /.direct-chat-msg -->
-
-                  <!-- Message to the right -->
-                  <div class="direct-chat-msg right">
-                    <div class="direct-chat-infos clearfix">
-                      <span class="direct-chat-name float-right">Sarah Bullock</span>
-                      <span class="direct-chat-timestamp float-left">23 Jan 2:05 pm</span>
-                    </div>
-                    <!-- /.direct-chat-infos -->
-                    <img class="direct-chat-img" src="dist/img/user3-128x128.jpg" alt="message user image">
-                    <!-- /.direct-chat-img -->
-                    <div class="direct-chat-text">
-                      You better believe it!
-                    </div>
-                    <!-- /.direct-chat-text -->
-                  </div>
-                  <!-- /.direct-chat-msg -->
-
-                  <!-- Message. Default to the left -->
-                  <div class="direct-chat-msg">
-                    <div class="direct-chat-infos clearfix">
-                      <span class="direct-chat-name float-left">Alexander Pierce</span>
-                      <span class="direct-chat-timestamp float-right">23 Jan 5:37 pm</span>
-                    </div>
-                    <!-- /.direct-chat-infos -->
-                    <img class="direct-chat-img" src="dist/img/user1-128x128.jpg" alt="message user image">
-                    <!-- /.direct-chat-img -->
-                    <div class="direct-chat-text">
-                      Working with AdminLTE on a great new app! Wanna join?
-                    </div>
-                    <!-- /.direct-chat-text -->
-                  </div>
-                  <!-- /.direct-chat-msg -->
-
-                  <!-- Message to the right -->
-                  <div class="direct-chat-msg right">
-                    <div class="direct-chat-infos clearfix">
-                      <span class="direct-chat-name float-right">Sarah Bullock</span>
-                      <span class="direct-chat-timestamp float-left">23 Jan 6:10 pm</span>
-                    </div>
-                    <!-- /.direct-chat-infos -->
-                    <img class="direct-chat-img" src="dist/img/user3-128x128.jpg" alt="message user image">
-                    <!-- /.direct-chat-img -->
-                    <div class="direct-chat-text">
-                      I would love to.
-                    </div>
-                    <!-- /.direct-chat-text -->
-                  </div>
-                  <!-- /.direct-chat-msg -->
-
-                </div>
-                <!--/.direct-chat-messages-->
-
-                <!-- Contacts are loaded here -->
-                <div class="direct-chat-contacts">
-                  <ul class="contacts-list">
-                    <li>
-                      <a href="#">
-                        <img class="contacts-list-img" src="dist/img/user1-128x128.jpg" alt="User Avatar">
-
-                        <div class="contacts-list-info">
-                          <span class="contacts-list-name">
-                            Count Dracula
-                            <small class="contacts-list-date float-right">2/28/2015</small>
-                          </span>
-                          <span class="contacts-list-msg">How have you been? I was...</span>
-                        </div>
-                        <!-- /.contacts-list-info -->
-                      </a>
-                    </li>
-                    <!-- End Contact Item -->
-                    <li>
-                      <a href="#">
-                        <img class="contacts-list-img" src="dist/img/user7-128x128.jpg" alt="User Avatar">
-
-                        <div class="contacts-list-info">
-                          <span class="contacts-list-name">
-                            Sarah Doe
-                            <small class="contacts-list-date float-right">2/23/2015</small>
-                          </span>
-                          <span class="contacts-list-msg">I will be waiting for...</span>
-                        </div>
-                        <!-- /.contacts-list-info -->
-                      </a>
-                    </li>
-                    <!-- End Contact Item -->
-                    <li>
-                      <a href="#">
-                        <img class="contacts-list-img" src="dist/img/user3-128x128.jpg" alt="User Avatar">
-
-                        <div class="contacts-list-info">
-                          <span class="contacts-list-name">
-                            Nadia Jolie
-                            <small class="contacts-list-date float-right">2/20/2015</small>
-                          </span>
-                          <span class="contacts-list-msg">I'll call you back at...</span>
-                        </div>
-                        <!-- /.contacts-list-info -->
-                      </a>
-                    </li>
-                    <!-- End Contact Item -->
-                    <li>
-                      <a href="#">
-                        <img class="contacts-list-img" src="dist/img/user5-128x128.jpg" alt="User Avatar">
-
-                        <div class="contacts-list-info">
-                          <span class="contacts-list-name">
-                            Nora S. Vans
-                            <small class="contacts-list-date float-right">2/10/2015</small>
-                          </span>
-                          <span class="contacts-list-msg">Where is your new...</span>
-                        </div>
-                        <!-- /.contacts-list-info -->
-                      </a>
-                    </li>
-                    <!-- End Contact Item -->
-                    <li>
-                      <a href="#">
-                        <img class="contacts-list-img" src="dist/img/user6-128x128.jpg" alt="User Avatar">
-
-                        <div class="contacts-list-info">
-                          <span class="contacts-list-name">
-                            John K.
-                            <small class="contacts-list-date float-right">1/27/2015</small>
-                          </span>
-                          <span class="contacts-list-msg">Can I take a look at...</span>
-                        </div>
-                        <!-- /.contacts-list-info -->
-                      </a>
-                    </li>
-                    <!-- End Contact Item -->
-                    <li>
-                      <a href="#">
-                        <img class="contacts-list-img" src="dist/img/user8-128x128.jpg" alt="User Avatar">
-
-                        <div class="contacts-list-info">
-                          <span class="contacts-list-name">
-                            Kenneth M.
-                            <small class="contacts-list-date float-right">1/4/2015</small>
-                          </span>
-                          <span class="contacts-list-msg">Never mind I found...</span>
-                        </div>
-                        <!-- /.contacts-list-info -->
-                      </a>
-                    </li>
-                    <!-- End Contact Item -->
-                  </ul>
-                  <!-- /.contacts-list -->
-                </div>
-                <!-- /.direct-chat-pane -->
-              </div>
-              <!-- /.card-body -->
-              <div class="card-footer">
-                <form action="#" method="post">
-                  <div class="input-group">
-                    <input type="text" name="message" placeholder="Type Message ..." class="form-control">
-                    <span class="input-group-append">
-                      <button type="button" class="btn btn-primary">Send</button>
-                    </span>
-                  </div>
-                </form>
-              </div>
-              <!-- /.card-footer-->
-            </div>
-            <!--/.direct-chat -->
 
             <!-- TO DO List -->
             <div class="card">
